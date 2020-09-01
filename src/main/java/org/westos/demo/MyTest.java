974eae6ccdaab61ff0ad1772e39943b8fcbfb11f
ShawnYue08 <1625454122@qq.com>
2020-09-01 15:19:42 +0800

    提交MyTest.java文件

---
+++ src/main/java/org/westos/demo/MyTest.java
@@ -0,0 +1,11 @@
+package org.westos.demo;
+
+/**
+ * @author lwj
+ * @date 2020/9/1 15:18
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("新增文件，commit提交到本地并push到远程仓库。");
+    }
+}
