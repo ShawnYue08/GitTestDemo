c73464f1295b30c0daf1d30b9e61ac0a57ea092d
ShawnYue08 <1625454122@qq.com>
2020-09-04 18:21:27 +0800

    新建了一个分支dev1和Video.java

---
+++ src/main/java/org/westos/demo1/Video.java
@@ -0,0 +1,11 @@
+package org.westos.demo1;
+
+/**
+ * @author lwj
+ * @date 2020/9/4 18:17
+ */
+public class Video {
+    public static void main(String[] args) {
+        System.out.println("dev1分支下的Video视屏功能");
+    }
+}
